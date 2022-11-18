bc90bf703430634ba954b57f45c23b5f3b5bda7a
Ginsway <ginsway@qq.com>
2022-11-18 16:47:01 +0800

    Alpha-5.5

--- src/main/java/com/ginsway/formula_addition/mixin/FormulaAdditionMixin.java
+++ src/main/java/com/ginsway/formula_addition/mixin/FormulaAdditionMixin.java
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class FormulaAdditionMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		FormulaAddition.LOGGER.info("This line is printed by an example mod mixin!");
+		FormulaAddition.LOGGER.info("This line is printed by formula_addition mod mixin!");
 	}
 }
